33b774399f506cb8dd00c0acc28d9e1f9ce38bed
ken7875 <ken19970723@gmail.com>
2026-01-26 00:50:09 +0800

    feat: 錯誤頁面優化

--- error.vue
+++ error.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <p>loading...</p>
+  <div class="flex flex-col items-center w-full h-screen mt-[30%]">
+    <h2 class="text-[70px] text-primary">錯誤</h2>
+    <p class="text-[50px] mb-[20px]">{{ $props.error?.message }}</p>
+    <BaseButton styleType="cancel" @click="backToMeetPage">返回</BaseButton>
   </div>
 </template>
 
@@ -14,4 +16,8 @@ console.error('my date web', props.error);
 if (props.error?.code === 401) {
   clearError({ redirect: '/auth/login' });
 }
+
+const backToMeetPage = () => {
+  clearError({ redirect: '/meet' });
+};
 </script>
